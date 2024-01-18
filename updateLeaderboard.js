6b79e7de176c997d0909d8ab67f94ecd581933ee
fromddy <ddy_mainland@163.com>
2024-01-18 11:56:38 +0800

    feat: backend server

--- updateLeaderboard.js
+++ updateLeaderboard.js
@@ -1,29 +1,37 @@
 const fs = require('fs')
-
+require('dotenv').config();
 const { ethers } = require("ethers")
-const dfABI = require('@darkforest_eth/contracts/abis/DarkForest.json')
-const contractAddress = '0x094a1729e15f67d66a0ccc8cbf485ace4efb0f91'
+const dfABI = require('./DarkForest.json')
 const axios = require('axios')
 
+const { DEFAULT_RPC, GAME_ADDRESS } = process.env;
 
 
-const provider = new ethers.providers.JsonRpcProvider('https://poa-xdai.gateway.pokt.network/v1/lb/620c7e1399eef60039c2de86')
+const contractAddress = GAME_ADDRESS;
+const rpcUrl = DEFAULT_RPC; 
 
-const decodePlayer = (player, addressToTwitter) => {
+// console.log('---------------------------')
+// console.log(contractAddress);
+// console.log(rpcUrl);
+// console.log('---------------------------')
 
-    console.log(player[1], addressToTwitter[player[1].toLowerCase()])
+const provider = new ethers.providers.JsonRpcProvider(rpcUrl)
 
-    return {
-        ethAddress: player[1],
-        score: player[5].toNumber(),
-        twitter: addressToTwitter[player[1].toLowerCase()]
-    }
-}
+// const decodePlayer = (player, addressToTwitter) => {
 
-const getAllTwitter = async () => {
-    const { data } = await axios.get('https://api.zkga.me/twitter/all-twitters')
-    return data
-}
+//     console.log(player[1], addressToTwitter[player[1].toLowerCase()])
+
+//     return {
+//         ethAddress: player[1],
+//         score: player[5].toNumber(),
+//         twitter: addressToTwitter[player[1].toLowerCase()]
+//     }
+// }
+
+// const getAllTwitter = async () => {
+//     const { data } = await axios.get('https://api.zkga.me/twitter/all-twitters')
+//     return data
+// }
 
 const updateLeaderboard = async () => {
     const df = new ethers.Contract(contractAddress, dfABI, provider)
@@ -34,9 +42,25 @@ const updateLeaderboard = async () => {
 
     const rawPlayers = await df.bulkGetPlayers(0, numOfPlayer)
 
-    const addressToTwitter = await getAllTwitter()
+    // const addressToTwitter = await getAllTwitter()
+    // rawPlayers.map(player => decodePlayer(player, addressToTwitter)).sort((a,b) => b.score - a.score)
+
+    const entries = [];
     
-    const entries = rawPlayers.map(player => decodePlayer(player, addressToTwitter)).sort((a,b) => b.score - a.score)
+    for(const player of rawPlayers){
+        let address = player[1]
+        let score = await df.getScore(address);
+        let scoreStr = score.toString();
+        if(scoreStr === '115792089237316195423570985008687907853269984665640564039457584007913129639935') score =undefined;
+        else score = score.toNumber();
+
+        console.log(address,' ',score);
+        let tmp = {ethAddress: address.toLowerCase(),score: score}
+
+        entries.push(tmp);
+       
+    }
+    
 
     fs.writeFileSync('./leaderboard.json', JSON.stringify({timestamp: Date.now(), entries}))
 }
